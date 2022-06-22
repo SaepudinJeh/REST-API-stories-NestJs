3c50e5876fbcac9b0494d561b3ca8074907d7c09
saepudin <agung.saepudin@ist.id>
2022-06-22 16:01:53 +0700

    update cors origin

--- src/main.ts
+++ src/main.ts
@@ -10,17 +10,22 @@ import * as helmet from 'helmet';
 // import * as csurf from 'csurf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
 
   app.use(compression());
   app.use(helmet.default());
   // app.use(csurf());
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS'
+  });
 
   const configSwagger = new DocumentBuilder()
     .addServer('http://localhost:3000')
+    .addServer('https://story-wkwk-app.herokuapp.com')
     .setTitle('The REST API Stories')
     .setDescription(
       'Everyone can print their own history and everyone has rights for himself and what he will do for himself in the future',
